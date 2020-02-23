4b64a7416de0dbd63de2b3712e038d38652f2808
hamadmarri <hamad.s.almarri@gmail.com>
2020-02-23 21:31:37 +1100

    enhance comments, auto same key bindings uncomment

--- plugins/codecomment/codecomment2.py
+++ plugins/codecomment/codecomment2.py
@@ -1,31 +1,41 @@
 #
-#### Author: Hamad Al Marri <hamad.s.almarri@gmail.com>
-#### Date: Feb 19th, 2020
+#  Editted By: Hamad Al Marri <hamad.s.almarri@gmail.com>
+#  Date: Feb 19th, 2020
 #
 #
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
+#  Code comment plugin
+#  This file is part of gedit
 #
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
+#  Copyright (C) 2005-2006 Igalia
+#  Copyright (C) 2006 Matthew Dugan
+#  Copyrignt (C) 2007 Steve Frécinaux
 #
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
 #
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 #
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301, USA.
+
+
 
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk
 
+from .codecomment_tags import CodeCommentTags
 from . import commands
 
-class Plugin():
+class Plugin(CodeCommentTags):
 	
 
 	def __init__(self, app):
@@ -35,7 +45,6 @@ class Plugin():
 		self.signal_handler = app.signal_handler
 		self.commands = []
 	
-	# do not remove 
 	def activate(self):
 		self.signal_handler.key_bindings_to_plugins.append(self)
 #		commands.set_commands(self)
@@ -48,81 +57,81 @@ class Plugin():
 			self.do_comment()
 			
 			
-			
-			
+
 
 	def do_comment(self):
 		sourceview = self.plugins["files_manager.files_manager"].current_file.source_view
 		buffer = sourceview.get_buffer()
 		
+		lang = buffer.get_language()
+		if lang is None:
+			return
 		
-		
+		# get tags from .lan file related to current files
+		# if .c file opened (tags are /* */ or //)
+		# if .py file opened (tags #) and so on
+		(start_tag, end_tag) = self.get_comment_tags(lang)
+		if not start_tag and not end_tag:
+			return
+			
+			
+		# get user selection
 		sel = buffer.get_selection_bounds()
 		currentPosMark = buffer.get_insert()
-		deselect = False
+		
+		# if user selected chars or multilines
 		if sel != ():
+			deselect = False
 			(start, end) = sel
-			if start.ends_line():
-				start.forward_line()
-			elif not start.starts_line():
+			if not start.starts_line():
 				start.set_line_offset(0)
-			if end.starts_line():
-				end.backward_char()
-			elif not end.ends_line():
+			if not end.ends_line():
 				end.forward_to_line_end()
-		else:
+		
+		# if user not selecting any chars (i.e. just placed cursor in a line)
+		else:	
 			deselect = True
 			start = buffer.get_iter_at_mark(currentPosMark)
+			oldPos = buffer.get_iter_at_mark(currentPosMark).get_offset()
 			start.set_line_offset(0)
 			end = start.copy()
 			end.forward_to_line_end()
 
-		lang = buffer.get_language()
-		if lang is None:
-			return
+		
+		new_code = self.add_comment_characters(buffer, start_tag, end_tag, start, end)
 
-		(start_tag, end_tag) = self.get_comment_tags(lang)
-
-		if not start_tag and not end_tag:
-			return
-
-		new_code = self.add_comment_characters(buffer,
-                                                   start_tag,
-                                                   end_tag,
-                                                   start,
-                                                   end)
-
+		# place the cursor to its old position
 		if deselect:
-			oldPosIter = buffer.get_iter_at_mark(currentPosMark)
-			buffer.select_range(oldPosIter,oldPosIter)
+			oldPosIter = buffer.get_iter_at_offset(oldPos + 2)
 			buffer.place_cursor(oldPosIter)
 
-
 	
 	
-	
-	
-	
-	def get_comment_tags(self, lang):
-		(s, e) = self.get_line_comment_tags(lang)
-		if (s, e) == (None, None):
-			(s, e) = self.get_block_comment_tags(lang)
-		return (s, e)
-
-
-	def get_block_comment_tags(self, lang):
-		start_tag = lang.get_metadata('block-comment-start')
-		end_tag = lang.get_metadata('block-comment-end')
-		if start_tag and end_tag:
-			return (start_tag, end_tag)
-		return (None, None)
+	def discard_white_spaces(self, iter):
+		count = 0
+		while not iter.ends_line():
+			# get char where the current iter pointing to
+			c = iter.get_char()
+			
+			# check if c is not white space
+			if c != " " and c != "\t":
+				return (iter, count)
+			
+			iter.forward_char()
+			count += 1
 		
+		return (iter, 0)
 		
-	def get_line_comment_tags(self, lang):
-		start_tag = lang.get_metadata('line-comment-start')
-		if start_tag:
-			return (start_tag, None)
-		return (None, None)
+	
+	def is_commented(self, comment_pos_iter, start_tag):
+		head_iter = comment_pos_iter.copy()
+		self.forward_tag(head_iter, start_tag)
+		s = comment_pos_iter.get_slice(head_iter)
+		if s == start_tag:
+			return True
+		
+		return False
+
 
 
 	def add_comment_characters(self, document, start_tag, end_tag, start, end):
@@ -130,21 +139,48 @@ class Plugin():
 		imark = document.create_mark("iter", start, False)
 		emark = document.create_mark("end", end, False)
 		number_lines = end.get_line() - start.get_line() + 1
+		comment_pos_iter = None
+		count = 0
 		
 		document.begin_user_action()
 
 		for i in range(0, number_lines):
 			iter = document.get_iter_at_mark(imark)
 			if not iter.ends_line():
-				document.insert(iter, start_tag)
-				if end_tag is not None:
+				
+				if not comment_pos_iter:
+					(comment_pos_iter, count) = self.discard_white_spaces(iter)
+					
+					# check if already commented
+					if self.is_commented(comment_pos_iter, start_tag):
+						new_code = self.remove_comment_characters(document, start_tag, end_tag, start, end)
+						return
+						
+				else:
+					comment_pos_iter = iter
+					# move iter to match first alignment
+					for i in range(count):
+						iter.forward_char()
+					
+				document.insert(comment_pos_iter, start_tag)
+				
+				# also insert a space
+				document.insert(comment_pos_iter, " ")
+				
+				# if block tag (/*    */) style
+				if end_tag:
+					# if not the last selected line
 					if i != number_lines -1:
+						# place the end block tag (*/) to end of line
 						iter = document.get_iter_at_mark(imark)
 						iter.forward_to_line_end()
 						document.insert(iter, end_tag)
 					else:
+						# place the end block tag to end of selection
 						iter = document.get_iter_at_mark(emark)
 						document.insert(iter, end_tag)
+					
+						
 			iter = document.get_iter_at_mark(imark)
 			iter.forward_line()
 			document.delete_mark(imark)
@@ -155,59 +191,78 @@ class Plugin():
 		document.delete_mark(imark)
 		new_start = document.get_iter_at_mark(smark)
 		new_end = document.get_iter_at_mark(emark)
-		if not new_start.ends_line():
-			self.backward_tag(new_start, start_tag)
+		# if not new_start.ends_line():
+		#	self.backward_tag(new_start, start_tag)
 		document.select_range(new_start, new_end)
 		document.delete_mark(smark)
 		document.delete_mark(emark)
 		
 	
 	
+	def forward_tag(self, iter, tag):
+		iter.forward_chars(len(tag))
+
 	def backward_tag(self, iter, tag):
 		iter.backward_chars(len(tag))
 		
 		
 		
 		
-#		new_code = self.remove_comment_characters(document,
-#                                                      start_tag,
-#                                                      end_tag,
-#                                                      start,
-#                                                      end)
 
 
 
-#def remove_comment_characters(self, document, start_tag, end_tag, start, end):
-#        smark = document.create_mark("start", start, False)
-#        emark = document.create_mark("end", end, False)
-#        number_lines = end.get_line() - start.get_line() + 1
-#        iter = start.copy()
-#        head_iter = iter.copy()
-#        self.forward_tag(head_iter, start_tag)
-
-#        document.begin_user_action()
-
-#        for i in range(0, number_lines):
-#            if self.get_tag_position_in_line(start_tag, head_iter, iter):
-#                dmark = document.create_mark("delete", iter, False)
-#                document.delete(iter, head_iter)
-#                if end_tag is not None:
-#                    iter = document.get_iter_at_mark(dmark)
-#                    head_iter = iter.copy()
-#                    self.forward_tag(head_iter, end_tag)
-#                    if self.get_tag_position_in_line(end_tag, head_iter, iter):
-#                        document.delete(iter, head_iter)
-#                document.delete_mark(dmark)
-#            iter = document.get_iter_at_mark(smark)
-#            iter.forward_line()
-#            document.delete_mark(smark)
-#            head_iter = iter.copy()
-#            self.forward_tag(head_iter, start_tag)
-#            smark = document.create_mark("iter", iter, True)
-
-#        document.end_user_action()
-
-#        document.delete_mark(smark)
-#        document.delete_mark(emark)
         
-        
+
+	def get_tag_position_in_line(self, tag, head_iter, iter):
+		while not iter.ends_line():
+			# get text starting from iter to head_iter
+			s = iter.get_slice(head_iter)
+			if s == tag:
+				return True
+			else:
+				head_iter.forward_char()
+				iter.forward_char()
+		return False
+		
+		
+	def remove_comment_characters(self, document, start_tag, end_tag, start, end):
+		smark = document.create_mark("start", start, False)
+		emark = document.create_mark("end", end, False)
+		number_lines = end.get_line() - start.get_line() + 1
+		iter = start.copy()
+		head_iter = iter.copy()
+		self.forward_tag(head_iter, start_tag)
+
+		document.begin_user_action()
+
+		for i in range(0, number_lines):
+			if self.get_tag_position_in_line(start_tag, head_iter, iter):
+				dmark = document.create_mark("delete", iter, False)
+				document.delete(iter, head_iter)
+				
+				# delete the extra space added
+				space_iter = head_iter.copy()
+				space_iter.forward_char()
+				s = head_iter.get_slice(space_iter)
+				if s == " ":
+					# remove 
+					document.delete(head_iter, space_iter)
+				
+				if end_tag is not None:
+					iter = document.get_iter_at_mark(dmark)
+					head_iter = iter.copy()
+					self.forward_tag(head_iter, end_tag)
+					if self.get_tag_position_in_line(end_tag, head_iter, iter):
+						document.delete(iter, head_iter)
+				document.delete_mark(dmark)
+			iter = document.get_iter_at_mark(smark)
+			iter.forward_line()
+			document.delete_mark(smark)
+			head_iter = iter.copy()
+			self.forward_tag(head_iter, start_tag)
+			smark = document.create_mark("iter", iter, True)
+
+		document.end_user_action()
+
+		document.delete_mark(smark)
+		document.delete_mark(emark)
