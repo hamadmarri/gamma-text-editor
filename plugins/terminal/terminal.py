d6b4048e492f6f82eab4b5f8a666822bf2a64156
hamadmarri <hamad.s.almarri@gmail.com>
2020-03-01 01:39:21 +1100

    update readme

--- plugins/terminal/terminal.py
+++ plugins/terminal/terminal.py
@@ -1,21 +1,31 @@
+# terminal.py - Embeded VTE terminal for gedit
+# This file is part of gedit
 #
-#### Author: Author: Hamad Al Marri <hamad.s.almarri@gmail.com>
+# Copyright (C) 2005-2006 - Paolo Borelli
+#
+# gedit is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# gedit is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with gedit; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor,
+# Boston, MA  02110-1301  USA
+#
+#
+#
+#### Edited by: Hamad Al Marri <hamad.s.almarri@gmail.com>
 #### Date: Feb 26th, 2020
-#
-#
-#	This program is free software: you can redistribute it and/or modify
-#	it under the terms of the GNU General Public License as published by
-#	the Free Software Foundation, either version 3 of the License, or
-#	(at your option) any later version.
-#
-#	This program is distributed in the hope that it will be useful,
-#	but WITHOUT ANY WARRANTY; without even the implied warranty of
-#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#	GNU General Public License for more details.
-#
-#	You should have received a copy of the GNU General Public License
-#	along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
+# 
+# 
+# 
+# 
 
 import gi
 gi.require_version('Gtk', '3.0')
